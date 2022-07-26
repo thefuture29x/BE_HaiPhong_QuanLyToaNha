bc2be8f65f32b0c9cd75e68a5c2aa5d87e602d6e
thefuture29x <teothiphong@gmail.com>
2022-07-26 13:31:53 +0700

    first commit

---
+++ src/main/java/com/example/be_haiphong_quanlytoanha/BeHaiPhongQuanLyToaNhaApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_haiphong_quanlytoanha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeHaiPhongQuanLyToaNhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeHaiPhongQuanLyToaNhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/be_haiphong_quanlytoanha/BeHaiPhongQuanLyToaNhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_haiphong_quanlytoanha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeHaiPhongQuanLyToaNhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
